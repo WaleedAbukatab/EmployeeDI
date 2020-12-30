337dc70832d7e107d00a3926b60da6d5f20defe2
Waleed <abukatab.waleed@gmail.com>
2020-12-30 09:09:34 +0200

    Add project files.

---
+++ Employee DI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Employee_DI.Startup))]
+namespace Employee_DI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
